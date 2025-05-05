95527a48a45626ca5a5ff110b46f0b643660520b
Omar Lizardo <olizardo@soc.ucla.edu>
2025-05-04 18:08:53 -0700

    a buncha

---
+++ old-stuff-slide.R
@@ -0,0 +1,64 @@
+<!--
+   
+   ## Correspondence Plot of Genre Column Scores
+   ![First and Second Dimensions](Plots/ca-genre-col12.png)
+
+## Correspondence Plot of Genre Column Scores
+![First and Third Dimensions](Plots/ca-genre-col13.png)
+
+## Correspondence Plot of Genre Column Scores
+![First and Fourth Dimensions](Plots/ca-genre-col14.png)
+
+## Correspondence Plot of Label Column Scores
+![First and Second Dimensions](Plots/ca-label-col12.png)
+
+## Correspondence Plot of Label Column Scores
+![First and Third Dimensions](Plots/ca-label-col13.png)
+
+## Correspondence Plot of Label Column Scores
+![First and Fourth Dimensions](Plots/ca-label-col14.png)
+
+## Correspondence Plot of Genre Column Scores
+![First and Second Dimensions](Plots/genre-sim-dens12.png)
+
+## Correspondence Plot of Label Column Scores
+![First and Second Dimensions](Plots/label-sim-dens12.png)
+
+
+## Correspondence Plot of Genre Clouds
+![First and Third Dimensions](Plots/genre-proto13.png)
+
+## Correspondence Plot of Genre Clouds
+![First and Fourth Dimensions](Plots/genre-proto14.png)
+
+## Correspondence Plot of Label Clouds
+![First and Second Dimensions](Plots/label-proto12-1.png)
+
+## Correspondence Plot of Label Clouds
+![First and Second Dimensions](Plots/label-proto12-2.png)
+
+## Correspondence Plot of Label Clouds
+![First and Third Dimensions](Plots/label-proto13.png)
+
+## Correspondence Plot of Label Clouds
+![First and Fourth Dimensions](Plots/label-proto14.png)
+
+## Genre Ridge Plot
+![First Dimension](Plots/genre-ridge-dim1.png)
+
+## Genre Ridge Plot
+![Second Dimension](Plots/genre-ridge-dim2.png)
+
+## Genre Ridge Plot
+![Third Dimension](Plots/genre-ridge-dim3.png)
+
+## Label Ridge Plot
+![First Dimension](Plots/label-ridge-dim1.png)
+
+## Label Ridge Plot
+![Second Dimension](Plots/label-ridge-dim2.png)
+
+## Label Ridge Plot
+![Third Dimension](Plots/label-ridge-dim3.png)
+
+-->
